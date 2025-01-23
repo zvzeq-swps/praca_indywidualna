3fe3041fd0adfc07cdfe1c6c4867d15395bde714
zvzeq-swps <zbednarek@st.swps.edu.pl>
2025-01-23 11:21:04 +0100

    lab_03

--- projekt/aplikacja/models.py
+++ projekt/aplikacja/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+# deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
+MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
 
-# Create your models here.
+SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=60)
+    country = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Person(models.Model):
+
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES, default=SHIRT_SIZES[0][0])
+    month_added = models.IntegerField(choices=MONTHS.choices, default=MONTHS.choices[0][0])
+    team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
--- projekt/projekt/settings.py
+++ projekt/projekt/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'aplikacja.apps.AplikacjaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
